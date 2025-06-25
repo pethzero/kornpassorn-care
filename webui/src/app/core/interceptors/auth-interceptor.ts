2284b744245c0fd575fb9a5a796a443e6d86c956
pethzero <thanadol6@gmail.com>
2025-06-25 19:05:31 +0700

    Update Angular API

--- webui/src/app/core/interceptors/auth-interceptor.ts
+++ webui/src/app/core/interceptors/auth-interceptor.ts
@@ -1,18 +1,35 @@
-// src/app/core/interceptors/auth-interceptor.ts
+// // src/app/core/interceptors/auth-interceptor.ts
 
+// import { HttpInterceptorFn } from '@angular/common/http';
+
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//   const token = localStorage.getItem('token');
+
+//   if (token) {
+//     const authReq = req.clone({
+//       setHeaders: {
+//         Authorization: `Bearer ${token}`
+//       }
+//     });
+//     return next(authReq);
+//   }
+
+//   return next(req);
+// };
+
+// src/app/core/interceptors/auth.interceptor.ts
+// src/app/core/interceptors/auth-interceptor.ts
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
-
   if (token) {
-    const authReq = req.clone({
+    const cloned = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(authReq);
+    return next(cloned);
   }
-
   return next(req);
 };
