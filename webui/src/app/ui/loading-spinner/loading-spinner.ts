558af1c61947b29587374777b71d61e2c6520a79
Pethzero <thanadol6@gmail.com>
2025-07-28 00:34:10 +0700

    Update

--- webui/src/app/ui/loading-spinner/loading-spinner.ts
+++ webui/src/app/ui/loading-spinner/loading-spinner.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
   templateUrl: './loading-spinner.html',
-  styleUrls: ['./loading-spinner.scss']
+  styleUrls: ['./loading-spinner.scss'],
+  imports: [CommonModule] // ✅ เพิ่มตรงนี้
 })
 export class LoadingSpinnerComponent {
-  @Input() size = 48; // px
+  @Input() size = 48;
   @Input() color = '#1976d2';
 }
