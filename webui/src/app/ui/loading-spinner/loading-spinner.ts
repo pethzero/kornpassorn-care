f80b17c29af8e2eba8b75c8050636c1770181fce
Pethzero <thanadol6@gmail.com>
2025-07-28 00:31:23 +0700

    snap

--- webui/src/app/ui/loading-spinner/loading-spinner.ts
+++ webui/src/app/ui/loading-spinner/loading-spinner.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
   templateUrl: './loading-spinner.html',
-  styleUrls: ['./loading-spinner.scss']
+  styleUrls: ['./loading-spinner.scss'],
+  imports: [CommonModule]
 })
 export class LoadingSpinnerComponent {
   @Input() size = 48; // px
